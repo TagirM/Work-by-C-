b4c3b910c6a377f7c96d4744fb4ca20b5eb32f59
Admin <tmurzagaleev@yandex.ru>
2022-08-06 23:02:36 +0700

    Condition

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    System.Console.WriteLine("Ура, это же МАША!");
+}
+else
+{
+    System.Console.Write("Привет, ");
+    System.Console.WriteLine(username);
+}
